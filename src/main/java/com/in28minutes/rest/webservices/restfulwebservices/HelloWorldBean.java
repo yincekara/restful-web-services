f73dbf0d6372d9ad69aeb83360a229b93576e0e4
yincekara <37476591+yincekara@users.noreply.github.com>
2019-03-26 11:26:37 +0300

    update

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+    private String message;
+    public HelloWorldBean(String message) {
+        this.message=message;
+    }
+
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("HelloWorldBean [message=%s]",message);
+        //return "HelloWorldBean{" + "message='" + message + '\'' + '}';
+    }
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //GET
+    //URI - /hello-world
+    //method - "Hello World
+
+    //@RequestMapping(method = RequestMethod.GET,path="/hello-world")
+    @GetMapping(path="/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+
+
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean ("Hello World");
+    }
+}

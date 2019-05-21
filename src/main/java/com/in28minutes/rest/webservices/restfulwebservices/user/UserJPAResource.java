dd2a347fd67ba74f801cfd2b34be6dde121a0f83
yincekara <37476591+yincekara@users.noreply.github.com>
2019-05-21 17:32:13 +0300

    update

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -18,6 +18,11 @@ public class UserJPAResource {
     @Autowired
     private UserRepository userRepository;
 
+
+    @Autowired
+    private PostRepository postRepository;
+
+
     @GetMapping("/jpa/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
@@ -67,6 +72,40 @@ public class UserJPAResource {
     @DeleteMapping("/jpa//users/{id}")
     public void deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
+    }
+
+
+
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> retrieveAllUsers(@PathVariable int id) {
+        Optional <User> userOptional =  userRepository.findById(id);
+        if (!userOptional.isPresent())
+        {
+            throw new UserNotFoundException("id-" + id);
+        }
+        return userOptional.get().getPosts();
+    }
+
+
+
+    @PostMapping("/jpa//users/{id}/posts")
+    public ResponseEntity<Object> createPost(@PathVariable int id, @RequestBody Post post) {
+        Optional <User> userOptional =  userRepository.findById(id);
+        if (!userOptional.isPresent())
+        {
+            throw new UserNotFoundException("id-" + id);
+        }
+        User user = userOptional.get();
+
+        post.setUser(user);
+
+        Post savedPost = postRepository.save(post);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedPost.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
